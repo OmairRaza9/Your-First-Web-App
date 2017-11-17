a412a78a3c7a8e9811925436d4a45b941367928e
Omair <omairraza9@gmail.com>
2017-11-16 19:05:08 -0500

    complete

--- server.rb
+++ server.rb
@@ -5,7 +5,28 @@ get '/home' do
   erb :index
 end
 
-get '/index' do
+get '/portfolio' do
 
   erb :gallery
+end
+
+get '/about_me' do
+  @skills = ['git', 'html', 'css', 'ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
+
+  erb :about_me
+end
+
+get '/favourites' do
+  @fav_links =  {'Facebook' => 'www.facebook.com', 'Google Finance' => 'www.google.com/finance', 'Youtube' => 'www.youtube.com' }
+
+erb :favourites
+end
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to '/portfolio'
 end
